eeb95175f95118d6740cec215d3d026a2783a848
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2023-01-24 22:14:44 +0100

    destructure arrays, the whole array in order.

---
+++ destructering-arrays/app.js
@@ -0,0 +1,5 @@
+const fruits = ["orange", "banana", "lemon"]
+const friends = ["ross", "monice", "phibie", "chandler", "rachel", "joey"]
+
+const [nerd, neatFreak, spookey, sarcastic, spoiled, idiot, janator] = friends
+console.log(nerd, spoiled, idiot, janator)
