03f8db71ca5f582c722eb4a69f572323eb06c920
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2023-01-25 19:14:34 +0100

    finish array destructuring. swap variables using array destructuring instead of traditional way.

--- destructering-arrays/app.js
+++ destructering-arrays/app.js
@@ -1,5 +1,15 @@
-const fruits = ["orange", "banana", "lemon"]
-const friends = ["ross", "monice", "phibie", "chandler", "rachel", "joey"]
+const fruits = ["orange", "banana", "lemon"];
+const friends = ["ross", "monice", "phibie", "chandler", "rachel", "joey"];
 
-const [nerd, neatFreak, spookey, sarcastic, spoiled, idiot, janator] = friends
-console.log(nerd, spoiled, idiot, janator)
+const [nerd, neatFreak, spookey, sarcastic, spoiled, idiot, janator] = friends;
+console.log(nerd, spoiled, idiot, janator);
+
+const [, , spook, , , dummy] = friends;
+console.log(spook, dummy);
+
+let looney = "chandler";
+let smartMouth = "phibie";
+
+[smartMouth, looney] = [looney, smartMouth];
+
+console.log(smartMouth, looney);
