1b7a492d41cb611ea896ffa238c61334fc248334
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2023-02-24 17:59:34 +0100

    finish spread operator nugget. finish object spread operator.

--- spread-operator/app.js
+++ spread-operator/app.js
@@ -1,6 +1,6 @@
 const kaffee = "kaffee";
 const letters = [...kaffee];
-console.log(letters);
+// console.log(letters);
 
 const boys = ["mike", "john", "tom"];
 const girls = ["lili", "sarah", "lilani"];
@@ -17,5 +17,12 @@ const friends = [...boys, bff, ...girls];
 // copy
 const newFriends = [...friends];
 newFriends[0] = "utena";
-console.log(newFriends);
-console.log(friends);
+// console.log(newFriends);
+// console.log(friends);
+
+// spread operator objects
+const person = { name: "gene", job: "berger boy" };
+const newPerson = { ...person, city: "new jersey", name: "louise" };
+
+console.log(person);
+console.log(newPerson);
