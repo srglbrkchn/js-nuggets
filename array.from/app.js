433116879493d5b3947dd6e891cf79d863c02a65
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2023-03-06 20:46:51 +0100

    create an array from scratch using Array.from() and an object with length of array specified in it.

--- array.from/app.js
+++ array.from/app.js
@@ -1,3 +1,18 @@
 const mango = "mango...";
+// console.log(Array.from(mango));
 
-console.log(Array.from(mango));
+const text = document.querySelectorAll(".text");
+// console.log(text);
+
+const newText = Array.from(text).find((item) => {
+  if (item.textContent === "person") {
+    return item;
+  }
+});
+// console.log(newText);
+
+const items = Array.from({ length: 120 }, (_, index) => {
+  return index;
+});
+
+console.log(items);
