33b4077c343e5a3b652ba067465ad352e79a8420
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2023-03-08 17:01:03 +0100

    final commit. populate the newItems array based on wanted pages with items from items array.

--- array.from/app.js
+++ array.from/app.js
@@ -15,4 +15,14 @@ const items = Array.from({ length: 120 }, (_, index) => {
   return index;
 });
 
-console.log(items);
+// the number of items per page we want
+const itemsPerPage = 8;
+const pages = Math.ceil(items.length / itemsPerPage);
+
+const newItems = Array.from({ length: pages }, (_, index) => {
+  const start = index * itemsPerPage;
+  const tempItems = items.slice(start, start + itemsPerPage);
+  return tempItems;
+});
+
+console.log(newItems);
