c3bccf4dcf1afdb727423658b839e992d01a37a7
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2023-03-09 12:42:39 +0100

    initial commit. start optional chaining nugget.

---
+++ optional-chaining/app.js
@@ -0,0 +1,14 @@
+const people = [
+  {
+    name: "Margarita",
+    location: { street: "6039 Margarita", timezone: { offset: "+9:30" } },
+  },
+  {
+    name: "Aiden",
+    location: { street: "4302 Fairview Road" },
+  },
+  {
+    name: "Elijah",
+    location: { street: "4478 Brick Kiln Road", timezone: { offset: "+5:45" } },
+  },
+];
